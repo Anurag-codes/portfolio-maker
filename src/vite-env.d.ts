9cc27dcdc3106b0998a48c79602e66f0268040a5
Anurag <anuragk5132@gmail.com>
2026-03-15 21:30:23 +0530

    error fix

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,7 +2,7 @@
 
 declare module 'gsap-trial/SplitText' {
   export class SplitText {
-    constructor(target: string | Element | Element[] | NodeList, vars?: Record<string, unknown>);
+    constructor(target: string | Element | (string | Element)[] | NodeList, vars?: Record<string, unknown>);
     chars: Element[];
     words: Element[];
     lines: Element[];
