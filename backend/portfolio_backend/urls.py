a6c42c168fe1cae38ce20cc99e2993c5336daeea
Anurag <anuragk5132@gmail.com>
2026-03-15 23:29:15 +0530

    error fix

--- backend/portfolio_backend/urls.py
+++ backend/portfolio_backend/urls.py
@@ -4,6 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('backend/admin/', admin.site.urls),
     path('api/', include('portfolio.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
