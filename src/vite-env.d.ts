a2316a60410430369edb2ca0ce5f2040f4fd5b9c
Anurag <anuragk5132@gmail.com>
2026-03-15 21:28:35 +0530

    error fix

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+declare module 'gsap-trial/SplitText' {
+  export class SplitText {
+    constructor(target: string | Element | Element[] | NodeList, vars?: Record<string, unknown>);
+    chars: Element[];
+    words: Element[];
+    lines: Element[];
+    revert(): void;
+    split(vars?: Record<string, unknown>): this;
+  }
+}
